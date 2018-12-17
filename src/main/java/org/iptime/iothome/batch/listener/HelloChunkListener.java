6c0896f8fe60fcea2f61694e9d8a10953a887604
jw <jw@jw-PC>
2018-12-17 17:36:48 +0900

    chuncklistener 추가

---
+++ src/main/java/org/iptime/iothome/batch/listener/HelloChunkListener.java
@@ -0,0 +1,28 @@
+package org.iptime.iothome.batch.listener;
+
+import org.iptime.iothome.batch.HelloItemProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ChunkListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
+
+public class HelloChunkListener implements ChunkListener{
+	
+	private Logger logger = LoggerFactory.getLogger(HelloChunkListener.class);
+	
+	@Override
+	public void beforeChunk(ChunkContext context) {
+		logger.debug("before Chunk");
+	}
+
+	@Override
+	public void afterChunk(ChunkContext context) {
+		logger.debug("after Chunk");
+	}
+
+	@Override
+	public void afterChunkError(ChunkContext context) {
+		logger.debug("after Chunk Error");
+	}
+
+}
