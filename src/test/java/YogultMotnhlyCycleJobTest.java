79c78fb0e1d75ea3b43fd9e5e2d7db864cafc2b9
jw <jw@jw-PC>
2018-12-26 11:30:05 +0900

    yogult spring batch monthly job test 추가

---
+++ src/test/java/YogultMotnhlyCycleJobTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"classpath:/org/iptime/iothome/config/spring/context.xml",
+                                  "classpath:/org/iptime/iothome/config/spring/batch-context.xml",
+                                  "classpath:/org/iptime/iothome/config/spring/batch-test-context.xml"})
+@EnableBatchProcessing
+public class YogultMotnhlyCycleJobTest {
+	
+	private Logger logger = LoggerFactory.getLogger(YogultMotnhlyCycleJobTest.class);
+
+	@Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+	
+	@Test
+	public void testYogultMotnhlyCycleJob() throws Exception {
+		/***given***/
+		
+		/***when***/
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		
+		/***then***/
+		assertEquals(new ExitStatus("COMPLETED"), jobExecution.getExitStatus());
+	}
+}
